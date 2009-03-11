4ba363dc4b3e4cf5c05f6a849746e60247e956a0
Glen Maddern <glen83@gmail.com>
2009-03-11 12:10:29 +1000

    file ordering seems protastic

--- spec/listing_spec.rb
+++ spec/listing_spec.rb
@@ -2,6 +2,6 @@ describe "Listing" do
   it "Should list within a specified directory" do
     #todo: make paths better
     files = `cd ~/src/box && rake with_dir=examples/one list | sed 1d`.gsub("examples/one/content/","").split("\n")
-    files.should == %w[!intro.markdown a/somefile.markdown b/anotherfile.markdown]
+    files.should == %w[!intro.markdown a/somefile.markdown b/anotherfile.markdown z.markdown]
   end
 end
