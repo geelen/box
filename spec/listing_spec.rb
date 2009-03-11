1c7956b4ee5a16fb86890f808264657e44dfc6d7
Glen Maddern <glen83@gmail.com>
2009-03-11 12:24:21 +1000

    both ways tested. i <3 ruby

--- spec/listing_spec.rb
+++ spec/listing_spec.rb
@@ -1,8 +1,19 @@
+def get_files cmd
+  `#{cmd} | grep -v '^(in '`.gsub(/^.*examples\/one\/content\//,"").split("\n")
+end
+
+def expected_files
+    #todo: should a.markdown precede a/somefile.markdown?
+    %w[!intro.markdown a/somefile.markdown a.markdown b/anotherfile.markdown z.markdown]
+end
+
 describe "Listing" do
   it "Should list within a specified directory" do
     #todo: make paths better
-    files = `cd ~/src/box && rake with_dir=examples/one list | sed 1d`.gsub("examples/one/content/","").split("\n")
-    #todo: should a.markdown precede a/somefile.markdown?
-    files.should == %w[!intro.markdown a/somefile.markdown a.markdown b/anotherfile.markdown z.markdown]
+    get_files("cd ~/src/box && rake with_dir=examples/one list").should == expected_files
+  end
+  
+  it "Should list from within the directory" do
+    get_files("cd examples/one && rake -f ~/src/box/Rakefile list").should == expected_files
   end
 end
