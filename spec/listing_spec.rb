1d283a2857a2bf114df519bc30d41060059b53ca
Glen Maddern <glen83@gmail.com>
2009-03-11 12:13:58 +1000

    Ordering is working, but maybe not how you'd expect.

--- spec/listing_spec.rb
+++ spec/listing_spec.rb
@@ -2,6 +2,7 @@ describe "Listing" do
   it "Should list within a specified directory" do
     #todo: make paths better
     files = `cd ~/src/box && rake with_dir=examples/one list | sed 1d`.gsub("examples/one/content/","").split("\n")
-    files.should == %w[!intro.markdown a/somefile.markdown b/anotherfile.markdown z.markdown]
+    #todo: should a.markdown precede a/somefile.markdown?
+    files.should == %w[!intro.markdown a/somefile.markdown a.markdown b/anotherfile.markdown z.markdown]
   end
 end
