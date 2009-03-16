d5f499df89faeb2899706c450c0e99b338dfc2de
Glen Maddern <glen83@gmail.com>
2009-03-16 11:15:34 +1000

    some failure checking

--- app/tasks.rb
+++ app/tasks.rb
@@ -14,6 +14,9 @@ end
 
 desc "Recompile the markdown to html"
 task :default => html_files do
+  raise "Directory doesn't exist!" if !File.exists? $working_dir
+  print bold, blue, "Running BOX in #{$working_dir}:", reset, "\n"
+  raise "No files to process!" if html_files.empty?
   do_all
   do_assets
 end
---
+++ spec/failues_spec.rb
@@ -0,0 +1,13 @@
+describe "Html Generator" do
+  it "should fail if path doesn't exist" do
+    str = `rake with_dir=examples/no_existo 2>> /dev/stdout`
+    str.should =~ /rake aborted!/
+    str.should =~ /Directory doesn't exist/
+  end
+  
+  it "should fail if path is empty" do
+    str = `cd examples/two && rake -f ../../box.rb 2>> /dev/stdout`
+    str.should =~ /rake aborted!/
+    str.should =~ /No files to process!/
+  end
+end
