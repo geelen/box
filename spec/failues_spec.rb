4cfeb2b127b1e305f3760031f238fc7ff12b97c7
Glen Maddern <glen83@gmail.com>
2009-03-16 11:16:29 +1000

    works with two fewer characters!

--- spec/failues_spec.rb
+++ spec/failues_spec.rb
@@ -1,12 +1,12 @@
 describe "Html Generator" do
   it "should fail if path doesn't exist" do
-    str = `rake with_dir=examples/no_existo 2>> /dev/stdout`
+    str = `rake with_dir=examples/no_existo 2> /dev/stdout`
     str.should =~ /rake aborted!/
     str.should =~ /Directory doesn't exist/
   end
   
   it "should fail if path is empty" do
-    str = `cd examples/two && rake -f ../../box.rb 2>> /dev/stdout`
+    str = `cd examples/two && rake -f ../../box.rb 2> /dev/stdout`
     str.should =~ /rake aborted!/
     str.should =~ /No files to process!/
   end
